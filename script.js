66e004d7cf039254313565d7adc562ee32e4fe58
Joseph Summerhays <summerhays.joseph@gmail.com>
2018-02-15 22:46:46 -0700

    api creative project

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
 $(document).ready(function() {
-  var submitButton = $('#weatherSubmit');
-  console.log(submitButton);
-  $('#weatherSubmit').click(function(e) {
-  	e.preventDefault();
-
-  	var value = $('#weatherInput').val();
-  	var myurl = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial"+"&APPID=18a552a5a757060623785a574d1c709a";
-    $.ajax({
-      url : myurl,
-      dataType : "json",
-      success : function(json) {
-        var results = "";
-    		results += '<h2>Weather in ' + json.name + "</h2>";
-    		for (var i=0; i<json.weather.length; i++) {
-    		    results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-    		}
-    		results += '<h2>' + json.main.temp + " &deg;F</h2>"
-    		results += "<p>"
-    		for (var i=0; i<json.weather.length; i++) {
-    		    results += json.weather[i].description
-    		    if (i !== json.weather.length - 1)
-    			results += ", "
-    		}
-    		results += "</p>";
-    		$("#weatherResults").html(results);
-      }
-    });
-  });
-  $('#stackSubmit').click(function(e) {
+  var myurl = "https://api.chucknorris.io/jokes/categories";
+  // console.log("hello, ");
+  // $.getJSON(myurl, function(result){
+  //   var htmlToPlace = '<ul class="navBar">';
+  //   /*var newList = document.createElement("ul");
+  //   newList.setAttribute('id', 'navBar');*/
+  //   for (var i = 1; i<result.length; i++) {
+  //     /*var listItem = document.createElement("li");
+  //     listItem.setAttribute('class', 'button');
+  //     listItem.appendChild(document.createTextNode(result[i]));
+  //     newList.appendChild(listItem);*/
+  //     htmlToPlace+= '<li class="button">' + result[i] + '</li>';
+  //   }
+  //   /*var navigate = document.getElementById("navigation");
+  //   navigate.appendChild(newList, navigate);*/
+  //   htmlToPlace += '</ul>';
+  //   $("#navigation").html(htmlToPlace);
+  //   console.log(htmlToPlace);
+  //   /*console.log(result);*/
+  //
+  // });
+  $('.button').click(function(e) {
+    console.log('flag 1');
     e.preventDefault();
-
-    var value = $('#stackInput').val();
-    var myurl = "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle="+ value + "&site=stackoverflow";
-    $.ajax({
-      url : myurl,
-      dataType : "json",
-      success : function(json) {
-        var results = "<h1>First Ten answers from stackoverflow</h1>";
-        for (var i=0; i<10; ++i) {
-          results += '<a href="' +json.items[i].link+'"><p>'+json.items[i].title+"</p></a>";
-        }
-        $('#stackResults').html(results);
-      }
+    console.log(e.target);
+    var jokeUrl = "https://api.chucknorris.io/jokes/random?category=" + $(e.target).text();
+    $.getJSON(jokeUrl, function(result) {
+      console.log('flag 2');
+      console.log(result);
+      var htmlToPlace = '<h1 id="joke">' + result.value + '</h1>';
+      $("#joke").html(htmlToPlace);
     });
   });
+  /*$.ajax({
+    /*url: myurl,
+    dataType : "json",
+    data: json,
+    success : function(json)
+    }*
+  });*/
 });
